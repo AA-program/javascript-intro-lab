66f746179c949c97e9575d29cc3c056464107dfb
AA-program <aarizviuk@hotmail.com>
2024-11-06 10:50:05 +0000

    Week 7

--- Js/myScript.js
+++ Js/myScript.js
@@ -13,10 +13,35 @@ console.log(firstButton); // Logs the first element with the class 'btn'
 let colourButtons = document.querySelectorAll(".colPicker");
 console.dir(colourButtons); // Logs a NodeList of all elements with the class 'colPicker'
 
-//colourButtons.forEach(button => {
-    button.style.backgroundColor = "red";
-//});
+for (let i = 0; i < colourButtons.length; i++){
+    colourButtons[i].addEventListener("click", chgColour)
+}
 
-for (let button of colourButtons) {
-    button.style.backgroundColor = "blue";
+function chgColour(ev){
+    console.info(ev.target.classList[0]);
+    let chosenColour = ev.target.classList[0] + "Back";
+    document.body.setAttribute("class", chosenColour);
+}
+
+let imageAr = [
+    'images/view1.jpg',
+    'images/view2.jpg',
+    'images/view3.jpg',
+    'images/view4.jpg',
+    'images/view5.jpg',
+    'images/view6.jpg',
+];
+
+let imageCounter = 0;
+let imageRotator = document.getElementById("myImages");
+setInterval(chgImage,3000);
+
+function chgImage(){
+    imageRotator.setAttribute("src", imageAr[imageCounter]);
+    if (imageCounter < 5){
+        imageCounter++;
+    }
+    else{
+        imageCounter = 0;
+    }
 }
